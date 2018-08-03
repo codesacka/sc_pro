e690a9e1f40cbd026b6145ad0007c7eedc1e2467
klunwebale <klunwebale@gmail.com>
2018-08-03 08:18:29 +0300

    Models Added

---
+++ app/Models/Financials/ExchangeRates.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Financials;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExchangeRates extends Model
+{
+    //
+}
---
+++ app/Models/Students/StudentEmergencyInformation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Students;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StudentEmergencyInformation extends Model
+{
+    //
+}
